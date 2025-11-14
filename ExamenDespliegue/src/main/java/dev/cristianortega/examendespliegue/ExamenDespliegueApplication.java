222c79328ec4c6500ac489bc2ab228eae62741cc
Cristian Ortega Gonzalez <146979141+Cristianortegaa@users.noreply.github.com>
2025-11-14 17:46:03 +0100

    Proyecto subido

---
+++ ExamenDespliegue/src/main/java/dev/cristianortega/examendespliegue/ExamenDespliegueApplication.java
@@ -0,0 +1,13 @@
+package dev.cristianortega.examendespliegue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenDespliegueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenDespliegueApplication.class, args);
+    }
+
+}
